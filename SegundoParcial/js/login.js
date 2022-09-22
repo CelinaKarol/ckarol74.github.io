5c422a26049361e1529e8cf1978db9d26fca4474
CelinaKarol <103543836+CelinaKarol@users.noreply.github.com>
2022-09-22 17:36:27 -0400

    Archivos innecesarios eliminados

--- SegundoParcial/js/login.js
+++
@@ -1,5 +0,0 @@
-function login(){
-    var nombre = document.getElementById('nombre').value; 
-    document.getElementById('mensaje').innerHTML= "Bienvenido! "+nombre;
-    // document.getElementById('dolares').value= "";
-}
